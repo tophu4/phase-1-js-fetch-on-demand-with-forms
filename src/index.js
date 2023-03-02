a40d9dd5420a148ac484fcbefc8f32471560c0c4
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-03-02 12:18:54 -0800

    add preventDefault to submit button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(event);
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
