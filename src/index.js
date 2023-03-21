30520a762b2e82264f0caba3e4b71e1fcfded3c6
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-03-21 12:43:47 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ const init = () => {
 
     inputForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log(event);
+        const input = document.querySelector("input#searchByID");
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector("section#movieDetails h4");
+                const summary = document.querySelector("section#movieDetails p");
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            });
+
+        console.log(input.value);
     });
 };
 
